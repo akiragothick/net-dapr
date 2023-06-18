91a5ae54991944244cc43d635a2c6676624637f9
Ernesto Vargas <titto_44@hotmail.com>
2023-06-18 02:45:04 -0500

    First version

---
+++ src/Test.Dapr.Client/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Test.Dapr.Client.Models;
+
+namespace Test.Dapr.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DaprClient daprClient;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(DaprClient daprClient, ILogger<HomeController> logger)
+        {
+            this.daprClient = daprClient;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var client = DaprClient.CreateInvokeHttpClient();
+                var forecasts1 = await client.GetFromJsonAsync<IEnumerable<WeatherForecast>>("http://mybackend/weatherforecast");
+
+                var forecasts2 = await daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                                   HttpMethod.Get, "mybackend", "weatherforecast");
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ src/readme.md
@@ -0,0 +1,17 @@
+# Levantar mediante comandos
+
+dapr run --app-id myfrontend --app-port 7001 --app-protocol http --dapr-http-port 3501 -- dotnet run
+
+dapr run --app-id mybackend --app-port 7002 --app-protocol http --dapr-http-port 3500 -- dotnet run
+
+dapr run --app-id myfrontend --app-port 7001 -- dotnet run
+
+dapr run --app-id mybackend --app-port 7002 -- dotnet run
+
+# Tambien disponible para levantar con docker compose
+
+
+
+# Llamada mediante http
+
+[GET] http://localhost:3500/v1.0/invoke/mybackend/method/weatherforecast
